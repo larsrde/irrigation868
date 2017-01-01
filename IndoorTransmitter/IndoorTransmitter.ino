6b718916083c6c9c6c7195b66e82ec4bc4ce3530
larsrde <lars@larsr.de>
2017-01-01 16:25:58 +0100

    Initial

---
+++ IndoorTransmitter/IndoorTransmitter.ino
@@ -0,0 +1,74 @@
+#include <RFM69.h>    //get it here: https://www.github.com/lowpowerlab/rfm69
+#include <SPI.h>
+#include <SPIFlash.h> //get it here: https://www.github.com/lowpowerlab/spiflash
+
+#define VALVECONTROLLERID  10
+#define INDOORTRANSMITTERID 50
+#define NETWORKID     103  //the same on all nodes that talk to each other
+//Match frequency to the hardware version of the radio on your Moteino (uncomment one):
+//#define FREQUENCY     RF69_433MHZ
+#define FREQUENCY     RF69_868MHZ
+//#define FREQUENCY     RF69_915MHZ
+#define ENCRYPTKEY    "ysdjklfhasipurz rzRZYSDFSAETQ§%& WERat" //exactly the same 16 characters/bytes on all nodes!
+//#define IS_RFM69HW    //uncomment only for RFM69HW! Leave out if you have RFM69W!
+#define ACK_TIME      30 // max # of ms to wait for an ack
+#define SERIAL_BAUD   9600
+
+#ifdef __AVR_ATmega1284P__
+  #define LED           15 // Moteino MEGAs have LEDs on D15
+#else
+  #define LED           9 // Moteinos have LEDs on D9
+#endif
+
+RFM69 radio;
+bool promiscuousMode = false; //set to 'true' to sniff all packets on the same network
+byte sendLen;
+char buffer[50];
+
+void setup() {
+  Serial.begin(SERIAL_BAUD);
+  delay(10);
+  radio.initialize(FREQUENCY,INDOORTRANSMITTERID,NETWORKID);
+#ifdef IS_RFM69HW
+  radio.setHighPower(); //only for RFM69HW!
+#endif
+  radio.encrypt(ENCRYPTKEY);
+  radio.promiscuous(promiscuousMode);
+  //radio.setFrequency(919000000);
+  char buff[50];
+  sprintf(buff, "\nListening at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+  Serial.println(buff);
+}
+
+byte ackCount=0;
+uint32_t packetCount = 0;
+
+void loop() {
+  //process any serial input
+  if (Serial.available() > 0)
+  {
+    char input = Serial.read();
+    if (input == 'r') //d=dump all register values
+      radio.readAllRegs();
+    if (input == 'E') //E=enable encryption
+      radio.encrypt(ENCRYPTKEY);
+    if (input == 'e') //e=disable encryption
+      radio.encrypt(null);
+    if(input == 's') {
+          sprintf(buffer, "Hallo");
+         sendLen = strlen(buffer);
+        radio.sendWithRetry(VALVECONTROLLERID, buffer, sendLen, 1);
+        Serial.println ("Sent Hallo");
+    }
+    Serial.println();
+    Blink(LED,3);
+  }
+}
+
+void Blink(byte PIN, int DELAY_MS)
+{
+  pinMode(PIN, OUTPUT);
+  digitalWrite(PIN,HIGH);
+  delay(DELAY_MS);
+  digitalWrite(PIN,LOW);
+}
